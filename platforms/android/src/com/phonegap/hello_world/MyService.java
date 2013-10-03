2ebd047df4e86b3caa7d5b65e22c520f43202c2f
Christoffer Rosen <cbr4830@rit.edu>
2013-10-02 23:21:40 -0400

    Background working, location is not.

--- platforms/android/src/com/phonegap/hello_world/MyService.java
+++ platforms/android/src/com/phonegap/hello_world/MyService.java
@@ -1,4 +1,4 @@
-package com.yournamespace.yourappname;
+package com.phonegap.hello_world;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -6,6 +6,12 @@ import java.util.Date;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
@@ -15,16 +21,72 @@ public class MyService extends BackgroundService {
 	private final static String TAG = MyService.class.getSimpleName();
 	
 	private String mHelloTo = "World";
+	private Location loc = null;
+	private boolean locationInit = false;
+	private LocationListener locationListener = null;
+	
+	// Acquire a reference to the system Location Manager
+	LocationManager locationManager;
+	
+	protected void locationUpdates(){
+		
+	}
 
+
+	protected void initLoc() {
+		Log.d(TAG,"init loc");
+		/*final Handler h = new Handler();
+		h.post(new Runnable() {
+			public void run() {
+				locationUpdates();
+				h.postDelayed(this, TIME)
+			}
+		})l */
+		
+		// Register the listener with the Location Manager to receive location updates
+		locationManager =  (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		// Define a listener that responds to location updates
+		locationListener = new LocationListener() {
+		    public void onLocationChanged(Location location) {
+		    	loc = location;
+		    	Log.d(TAG,"location change");
+		    }
+		    public void onStatusChanged(String provider, int status, Bundle extras) {
+		    	Log.d(TAG,"LOCATION ON STATUS CHANGED");
+		    }
+		    public void onProviderEnabled(String provider) {
+		    	Log.d(TAG,"LOCATION ON PROVIDER ENABLED");
+		    }
+
+		    public void onProviderDisabled(String provider) {
+		    	Log.d(TAG,"LOCATION ON PROVIDER DISABLED");
+		    }
+		};
+		
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener, getMainLooper());
+		locationInit = true;
+		
+		
+	}
 	@Override
-	protected JSONObject doWork() {
+	protected synchronized JSONObject doWork() {
 		JSONObject result = new JSONObject();
 		
+		if(!locationInit){
+			initLoc();
+		}
+		
 		try {
-			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
-			String now = df.format(new Date(System.currentTimeMillis())); 
-
-			String msg = "Hello " + this.mHelloTo + " - its currently " + now;
+			//SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
+			//String now = df.format(new Date(System.currentTimeMillis())); 
+			
+			String msg;
+			if(loc != null){
+				msg = "Lat " + loc.getLatitude() + " Long " +loc.getLongitude();
+			} else {
+				msg = "LOCATION IS NULL";
+			}
+		
 			result.put("Message", msg);
 
 			Log.d(TAG, msg);
